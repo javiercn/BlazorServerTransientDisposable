9ee2594edf5e8daa52af56d7fae1b666da049e50
Javier Calvarro Nelson <jacalvar@microsoft.com>
2020-05-20 12:02:29 -0700

    Add project files.

---
+++ BlazorServerTransientDisposable/ThrowOnTransientDisposableHandler.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components.Server.Circuits;
+
+namespace BlazorServerTransientDisposable
+{
+    internal class ThrowOnTransientDisposableHandler : CircuitHandler
+    {
+        public ThrowOnTransientDisposableHandler(ThrowOnTransientDisposable throwOnTransientDisposable)
+        {
+            throwOnTransientDisposable.ShouldThrow = true;
+        }
+    }
+}
